fe757198a0469bae0f8f17a218e22e7660ce28c3
ArmanO2005 <cyclanthaceae@berkeley.edu>
2024-08-04 10:59:32 -0700

    training set additions; code updates

--- main.py
+++ main.py
@@ -22,27 +22,57 @@ nltk.download('taggers')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('stopwords')
 
-lemmetizer = WordNetLemmatizer()
+lemmatizer = WordNetLemmatizer()
 
 tag_map = defaultdict(lambda : wn.NOUN)
 tag_map['J'] = wn.ADJ
 tag_map['V'] = wn.VERB
 tag_map['R'] = wn.ADV
 
-data = pd.read_csv("C://Users//arman//Dropbox//1 - App Dev//Chat AI stuff//training.csv")
-
 def remove_punctuation(tokens):
-    return [word for word in tokens if word not in string.punctuation]
+    return [word for word in tokens if any([i in string.punctuation for i in word]) == False]
+
 
 def tokenize(data):
-    for i, text in enumerate(data['Title']):
-        data.loc[i, 'Title'] = [word_tokenize(t) for t in sent_tokenize(text.lower())]
-
-    for i, text in enumerate(data['Title']):
-        for j, sentence in enumerate(text):
-            sentence = remove_punctuation(sentence)
-            data.loc[i, 'Title'][j] = [lemmetizer.lemmatize(word, tag_map[tag[0]]) for word, tag in nltk.pos_tag(sentence) if word not in stopwords.words('english')]
+    for i, document in enumerate(data['Title']):
+        data.loc[i, 'Title'] = [word_tokenize(t) for t in sent_tokenize(document.lower())]
     
+    for i, document in enumerate(data['Title']):    
+        data.loc[i, 'Title'] = [remove_punctuation(sentence) for sentence in document]
+
+def lemmatize(data):
+    for i, document in enumerate(data['Title']):
+        for j, sentence in enumerate(document):
+            sentence = remove_punctuation(sentence)
+            data.loc[i, 'Title'][j] = [lemmatizer.lemmatize(word, tag_map[tag[0]]) for word, tag in nltk.pos_tag(sentence) if word not in stopwords.words('english')]
+    
+def join_tokens(data):
+    for i, text in enumerate(data['Title']):
+        data.loc[i, 'Title'] = ' '.join([' '.join(sentence) for sentence in text])
+    
+
+data = pd.read_csv("C://Users//arman//Dropbox//1 - App Dev//Chat AI stuff//training.csv").dropna()
 tokenize(data)
-            
-print(data)
+
+data.to_csv('output.csv', index=False)
+
+
+# tfidf_vect = TfidfVectorizer(max_features=5000)
+# tfidf_matrix = tfidf_vect.fit_transform(data['Title'])
+
+# tfidf_vect.get_feature_names_out()
+
+# tfidf_df = pd.DataFrame(tfidf_matrix.toarray(), columns=tfidf_vect.get_feature_names_out())
+
+# tfidf_df.to_csv('output.csv', index=False)
+
+# X_train, X_test, y_train, y_test = model_selection.train_test_split(tfidf_matrix, data['Label'], test_size=0.3)
+
+
+# svm_model = svm.SVC(kernel='linear')
+# svm_model.fit(X_train, y_train)
+
+
+# predictions = svm_model.predict(X_test)
+# accuracy = accuracy_score(y_test, predictions)
+# print(f"Accuracy: {accuracy}")
